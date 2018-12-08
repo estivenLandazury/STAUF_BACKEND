1de52c3b30c05abdebe3cd4eb01bbc150e65674f
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2018-12-08 17:19:49 -0500

    update

--- Usuario/admin.py
+++ Usuario/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from Usuario.models import Usuarios
 from Usuario.models import TipoUsuario
-from django.contrib.auth.models import User
+from Usuario.models import Alarma
 
 # Aqui mostrará os modelos agregados    
 admin.site.register(Usuarios) 
 admin.site.register(TipoUsuario)
-admin.site.register(User)
+admin.site.register(Alarma)
