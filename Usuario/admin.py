4a10fb91a5e9065f25007691301d5ec201ec5755
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2018-12-08 17:12:39 -0500

    add user

--- Usuario/admin.py
+++ Usuario/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from Usuario.models import Usuarios
 from Usuario.models import TipoUsuario
+from Usuario.models import User
 
 
 # Register your models here.
 
 # Aqui mostrará os modelos agregados    
 admin.site.register(Usuarios) 
-admin.site.register(TipoUsuario) 
+admin.site.register(TipoUsuario)
+admin.site.register(User)
