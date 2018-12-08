f1b12d0c348ca189bd680bd25831228f285088e3
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2018-12-08 17:17:55 -0500

    Update

--- Usuario/admin.py
+++ Usuario/admin.py
@@ -3,9 +3,6 @@ from Usuario.models import Usuarios
 from Usuario.models import TipoUsuario
 from django.contrib.auth.models import User
 
-
-# Register your models here.
-
 # Aqui mostrará os modelos agregados    
 admin.site.register(Usuarios) 
 admin.site.register(TipoUsuario)
